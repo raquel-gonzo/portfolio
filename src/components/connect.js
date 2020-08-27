6e200ffc47f5e4a6be628e88247a5999f895cf2e
raquel-gonzo <gonzalez.rachel.c@gmail.com>
2020-08-26 21:22:13 -0500

    some responsive web design added. second commit.

--- src/components/connect.js
+++ src/components/connect.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
 const Connect = () => {
+    const linkedIn = () => {
+        window.open(
+            "https://www.linkedin.com/in/rgonzalezviolin"
+        );
+    }
+
+    const gitHub = () => {
+        window.open = () => {
+            "https://github.com/raquel-gonzo"
+        }
+    }
     return(
-        <div id="connect">
-            <img></img>
-        </div>
+        <>
+            <div id="connect">
+                <i onClick={linkedIn} className="fab fa-linkedin fa-3x"></i>  
+                <i onClick={gitHub} className="fab fa-github-square fa-3x"></i>
+            </div>
+        </>
     )
 }
 
---
+++ src/components/sheetmusic.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const SheetMusic = () => {
+    return (
+    <div>
+        <iframe
+        src="https://flat.io/embed/5f3eb48c06255f461a30bcfa?sharingKey=4e7f83062e67fff03b5b7ce545cc4395b92b10124b7a672bf1373bda16b69c5d3bf38057d911c158499f3b7bbb4080f51674b7972555add76f1d2ab45e3f91e1"
+        height="450"
+        width="750"
+        frameBorder="0"
+        allowfullscreen>
+        </iframe>
+    </div>
+    );
+};
+
+export default SheetMusic;
