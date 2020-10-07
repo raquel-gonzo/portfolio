468682f22afc690adf14a77254709feb6482625d
raquel-gonzo <gonzalez.rachel.c@gmail.com>
2020-10-06 22:27:31 -0500

    refactored app.js and project.js to use map function. moved projects into a json object.

--- src/App.js
+++ src/App.js
@@ -4,15 +4,36 @@ import Header from './components/header';
 import Project from './components/project';
 import Connect from './components/connect';
 
+const projects = [
+  {
+    title: "Violin Studio",
+    content: "a practice journal and sheet music resource."
+  },
+  {
+    title: "Yoga Lib",
+    content: "a library of yoga poses."
+  },
+  {
+    title: "proj 3",
+    content: ""
+  },
+]
+
 function App() {
+
+
   return (
     <div className="App">
       <Header/>
       <h2>My Code Projects</h2>
       <div id="app-container">
-        <Project />
-        <Project />
-        <Project />
+        {
+          projects.map((project, index) => {
+            console.log("project");
+            console.log(project);
+            return <Project project={project} key={index}/>
+          })
+        }
       </div>
       <Connect />
     </div>
