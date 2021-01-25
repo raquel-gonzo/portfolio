1a3e7d9ed52be1ee5abf0aa2ba7b9caebd80635f
raquel-gonzo <gonzalez.rachel.c@gmail.com>
2021-01-24 23:12:34 -0600

    added some dev icons, but they are not yet implemented. also added vue-weather project and updated some CSS. did this all on the devIcons branch whoops.

---
+++ src/components/DevIcons.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const DevIcons = () => {
+
+    return(
+        <div>
+        <i class="devicon-bootstrap-plain-wordmark colored"></i>
+        <i class="devicon-css3-plain-wordmark colored"></i>
+        <i class="devicon-dot-net-plain-wordmark colored"></i>
+        <i class="devicon-express-original-wordmark colored"></i>
+        <i class="devicon-git-plain-wordmark colored"></i>
+        <i class="devicon-github-original-wordmark colored"></i>
+        <i class="devicon-heroku-plain-wordmark colored"></i>
+        <i class="devicon-html5-plain-wordmark colored"></i>
+        <i class="devicon-javascript-plain colored"></i>
+        <i class="devicon-jquery-plain-wordmark colored"></i>
+        <i class="devicon-mongodb-plain-wordmark colored"></i>
+        <i class="devicon-mysql-plain-wordmark colored"></i>
+        <i class="devicon-nodejs-plain-wordmark colored"></i>
+        <i class="devicon-rails-plain-wordmark colored"></i>
+        <i class="devicon-react-original colored"></i>
+        <i class="devicon-ruby-plain-wordmark colored"></i>
+        <i class="devicon-vuejs-plain-wordmark colored"></i>
+        </div>
+    )
+}
+
+export default DevIcons;
